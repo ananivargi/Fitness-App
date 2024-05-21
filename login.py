fb48a4fa21625a2c093574013e4e0d69342ae0b8
ananivargi <ananya.nivargi@education.nsw.gov.au>
2024-05-21 20:17:59 +1000

    worked on home page

--- login.py
+++ login.py
@@ -1,53 +1,77 @@
+# Import relevant libraries
 import tkinter as tk
 from tkinter import messagebox
 
+#Check user information
 def check_login(username, password):
+    ''' 
+    Function receives the username and password entered in the 
+    login box and determines whether both individually are valid 
+    (they exist as part of one entry in the text file containing 
+    user information)
+    '''
+    # Initialise variable that stores whether username/password found
     value = ''
+    # Open user information file
     f = open('userlogininfo.txt', 'r')
     lines = f.readlines()
+    # Iterate through each line in file
     for line in lines:
+        # Check if the first item in line matches the username
         if line.split(';')[0] == username:
             value += 'username'
+            # Check if second item in line matches password
             if line.split(';')[1] == password:
                 value += 'password'
     f.close()
+    # Return which fields matched file entry
     return value
             
-def new_user(username, password,name, age, bmi, vo2):
+            
+def new_user(username,password,name, age, height, weight, vo2):
+    '''
+    Creates a new entry in the text file for new user profile
+    '''
     f = open('userlogininfo.txt', 'a')
-    f.write(f'{username};{password};{age};{bmi};{vo2}\n')
+    f.write(f'{username};{password};{age};{height};{weight};{vo2}\n')
     f.close()
     
-def check_valid_fields():
-    pass
+def check_valid_fields(username,password,name, age, height, weight, vo2):
+    '''
+    Checks whether the given info for each field is valid.
+    Username mustn't have any spaces, name must only be letters
+    Age must be number, at least 10
     
-# Dictionary to store user credentials
-userLogin = {}
+    '''
+    pass
+
 
 def login():
     username = username_entry.get()
     password = password_entry.get()
 
     if check_login(username, password) == 'usernamepassword':
-        messagebox.showinfo("Login Successful", "Welcome, {}".format(username))
+        #messagebox.showinfo("Login Successful", "Welcome, {}".format(username))
+        login_frame.pack_forget()
+        home_frame.pack()
     else:
         messagebox.showerror("Login Failed", "Please enter a valid username and password.")
 
 def register():
     def register_user():
-        print ("hello mf")
         username = new_username_entry.get()
         password = new_password_entry.get()
         name = name_entry.get()
         age = age_entry.get()
-        bmi = bmi_entry.get()
+        height = height_entry.get()
+        weight = weight_entry.get()
         vo2_max = vo2_max_entry.get()
 
-        if username and password and name and age and bmi and vo2_max:
+        if username and password and name and age and height and weight and vo2_max:
             if 'username' in check_login(username, password):
                 messagebox.showerror("Registration Failed", "Username already exists")
             else:
-                new_user(username,password,name,age,bmi,vo2_max)
+                new_user(username,password,name,age,height, weight, vo2_max)
 
                 messagebox.showinfo("Registration Successful", "Welcome, {}".format(username))
                 # Clear the entry fields after registration
@@ -55,7 +79,8 @@ def register():
                 new_password_entry.delete(0, tk.END)
                 name_entry.delete(0, tk.END)
                 age_entry.delete(0, tk.END)
-                bmi_entry.delete(0, tk.END)
+                height_entry.delete(0, tk.END)
+                weight_entry.delete(0, tk.END)
                 vo2_max_entry.delete(0, tk.END)
         else:
             messagebox.showerror("Registration Failed", "Please fill in all fields")
@@ -87,10 +112,15 @@ def register():
     age_entry = tk.Entry(register_frame, font=("Arial", 16))
     age_entry.grid(row=3, column=1, pady=10)
 
-    bmi_label = tk.Label(register_frame, text="BMI", bg='#333333', fg="#FFFFFF", font=("Arial", 16))
-    bmi_label.grid(row=4, column=0)
-    bmi_entry = tk.Entry(register_frame, font=("Arial", 16))
-    bmi_entry.grid(row=4, column=1, pady=10)
+    height_label = tk.Label(register_frame, text="Height", bg='#333333', fg="#FFFFFF", font=("Arial", 16))
+    height_label.grid(row=4, column=0)
+    height_entry = tk.Entry(register_frame, font=("Arial", 16))
+    height_entry.grid(row=4, column=1, pady=10)
+    
+    weight_label = tk.Label(register_frame, text="Weight", bg='#333333', fg="#FFFFFF", font=("Arial", 16))
+    weight_label.grid(row=4, column=0)
+    weight_entry = tk.Entry(register_frame, font=("Arial", 16))
+    weight_entry.grid(row=4, column=1, pady=10)
 
     vo2_max_label = tk.Label(register_frame, text="VO2 Max", bg='#333333', fg="#FFFFFF", font=("Arial", 16))
     vo2_max_label.grid(row=5, column=0)
@@ -108,20 +138,20 @@ window.title("BodyWise Fitness App")
 window.geometry('340x440')
 window.configure(bg='#333333')
 
-frame = tk.Frame(bg='#333333')
+login_frame = tk.Frame(bg='#333333')
 
 # Creating widgets
-login_label = tk.Label(frame, text="BodyWise", bg='#333333', fg="#468ce8", font=("Arial", 30))
-username_label = tk.Label(frame, text="Username", bg='#333333', fg="#FFFFFF", font=("Arial", 16))
-username_entry = tk.Entry(frame, font=("Arial", 16))
-password_entry = tk.Entry(frame, show="*", font=("Arial", 16))
-password_label = tk.Label(frame, text="Password", bg='#333333', fg="#FFFFFF", font=("Arial", 16))
+login_label = tk.Label(login_frame, text="BodyWise", bg='#333333', fg="#468ce8", font=("Arial", 30))
+username_label = tk.Label(login_frame, text="Username", bg='#333333', fg="#FFFFFF", font=("Arial", 16))
+username_entry = tk.Entry(login_frame, font=("Arial", 16))
+password_entry = tk.Entry(login_frame, show="*", font=("Arial", 16))
+password_label = tk.Label(login_frame, text="Password", bg='#333333', fg="#FFFFFF", font=("Arial", 16))
 
 #Login Button
-login_button = tk.Button(frame, text="Login", bg="#468ce8", fg="#FFFFFF", font=("Arial", 16), command=login)
+login_button = tk.Button(login_frame, text="Login", bg="#468ce8", fg="#FFFFFF", font=("Arial", 16), command=login)
 
 # Register Button
-register_button = tk.Button(frame, text="Register", bg="#468ce8", fg="#FFFFFF", font=("Arial", 16), command=register)
+register_button = tk.Button(login_frame, text="Register", bg="#468ce8", fg="#FFFFFF", font=("Arial", 16), command=register)
 
 # Placing widgets on the screen
 login_label.grid(row=0, column=0, columnspan=2, sticky="news", pady=40)
@@ -132,6 +162,11 @@ password_entry.grid(row=2, column=1, pady=20)
 login_button.grid(row=3, column=0, columnspan=2, pady=30)
 register_button.grid(row=4, column=0, columnspan=2, pady=10)
 
-frame.pack()
+home_frame = tk.Frame(bg='#223322')
+home_frame_label = tk.Label(home_frame, text="Welcome to the Homepage!")
+home_frame_label.pack(pady=20)
+
+login_frame.pack()
+home_frame.forget()
 
 window.mainloop()
